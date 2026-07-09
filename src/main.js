cdc48020b1ff250032d6acc8f674c7c7681acf0a
golsho <hpwdyvs72s@privaterelay.appleid.com>
2026-07-09 23:54:02 +0200

    images

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import "reveal.js/reveal.css";
 import "./style.css";
 import Reveal from "reveal.js";
 
-Reveal.initialize().then(() => {
+Reveal.initialize({
+  controls: false,
+  disableLayout: true,
+  hash: true,
+  mouseWheel: true,
+  progress: false,
+  transition: "slide",
+}).then(() => {
   document.querySelector(".reveal")?.classList.add("is-ready");
 });
